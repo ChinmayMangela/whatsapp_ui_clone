253336956ffd724a07a1037ecbfdd29812adc688
Chinmay Mangela <116483169+ChinmayMangela@users.noreply.github.com>
2024-08-10 13:12:51 +0530

    Update project

--- lib/presentation/pages/conversation_screen.dart
+++ lib/presentation/pages/conversation_screen.dart
@@ -5,7 +5,7 @@ import 'package:whatsapp_ui_clone/presentation/widgets/app_bar_icons.dart';
 import 'package:whatsapp_ui_clone/presentation/widgets/chats_list.dart';
 import 'package:whatsapp_ui_clone/presentation/widgets/custom_text.dart';
 
-class ConversationScreen extends StatelessWidget {
+class ConversationScreen extends StatefulWidget {
   const ConversationScreen({
     super.key,
     required this.user,
@@ -14,9 +14,21 @@ class ConversationScreen extends StatelessWidget {
   final User user;
 
   @override
+  State<ConversationScreen> createState() => _ConversationScreenState();
+}
+
+class _ConversationScreenState extends State<ConversationScreen> {
+  final _textFieldFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _textFieldFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: _buildAppBar(context),
       body: _buildBody(),
     );
@@ -28,11 +40,11 @@ class ConversationScreen extends StatelessWidget {
         onTap: () {
           Navigator.of(context).push(
             MaterialPageRoute(
-              builder: (context) => SpecificUserInfoPage(user: user),
+              builder: (context) => SpecificUserInfoPage(user: widget.user),
             ),
           );
         },
-        child: CustomText(text: user.name, textSize: 16, isBoldFont: true),
+        child: CustomText(text: widget.user.name, textSize: 16, isBoldFont: true),
       ),
       actions: [
         buildPhoneCallButton(),
@@ -42,10 +54,85 @@ class ConversationScreen extends StatelessWidget {
   }
 
   Widget _buildBody() {
-    return const Stack(
+    return Stack(
       children: [
-        Expanded(child: ChatsList()),
+        Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            )),
+        const ChatsList(),
+        Positioned(
+          bottom: 5,
+          left: 5,
+          right: 5,
+          child: Row(
+            children: [
+              Expanded(
+                child: _buildTextField(),
+              ),
+              _buildMicButton(),
+            ],
+          ),
+        ),
       ],
+    );
+  }
+
+  Widget _buildTextField() {
+    return TextField(
+      focusNode: _textFieldFocusNode,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: const Color.fromRGBO(10, 37, 46, 1.0),
+        prefixIcon: const Icon(Icons.emoji_emotions_outlined, color: Colors.grey,),
+        suffix: const Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Icon(Icons.attach_file_outlined, color: Colors.grey,),
+            SizedBox(width: 10),
+            Icon(Icons.camera_alt_outlined, color: Colors.grey,),
+          ],
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Color.fromRGBO(10, 37, 46, 1.0),
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Color.fromRGBO(10, 37, 46, 1.0)
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Color.fromRGBO(10, 37, 46, 1.0)
+          ),
+        ),
+        hintText: 'message...',
+      ),
+    );
+  }
+
+  Widget _buildMicButton() {
+    return Container(
+      margin: const EdgeInsets.only(left: 5),
+      decoration: const BoxDecoration(
+          color: Colors.green,
+          shape: BoxShape.circle
+      ),
+      child: IconButton(
+        icon: const Icon(Icons.mic_rounded, color: Colors.black,),
+        onPressed: () {
+        },
+      ),
     );
   }
 }
